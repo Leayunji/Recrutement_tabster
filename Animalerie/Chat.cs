8e072fba610b37c7eb2a61af8ef009dd1e6ab38c
Lea Tristan <lea.tristan@epita.fr>
2020-08-05 15:56:42 +0200

    [ANIMAL] ajout des classes Chien et Chat qui héritent de la classe Animal

---
+++ Animalerie/Chat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animalerie
+{
+    class Chat : Animal
+    {
+        public Chat()
+        {
+            Price = 110;
+            type_ = Type.Chat;
+        }
+
+        public override void Parle()
+        {
+            Console.WriteLine("Miaou");
+        }
+    }
+}
---
+++ Animalerie/Chien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animalerie
+{
+    class Chien : Animal
+    {
+        public Chien()
+        {
+            Price = 100;
+            type_ = Type.Chien;
+        }
+
+        public override void Parle()
+        {
+            Console.WriteLine("Ouaf");
+        }
+    }
+}
